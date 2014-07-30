f06b254382ee784b4600dc196dbe8dbd2d7c2535
Matt McKegg <matt@wetsand.co.nz>
2014-07-30 21:19:34 +1200

    speed, batch processing, only go 4 levels deep to avoid circular references

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
-var glob = require('glob')
+var Glob = require('glob').Glob
 var getDirectory = require('path').dirname
 var exec = require('child_process').execFile
 var Through = require('through')
 
-module.exports = function(path, cb){
+module.exports = function(root, cb){
   var stream = Through()
   var result = []
 
-  glob('**/.git', {cwd: path}, function(err, paths){
-    var pending = 0
-    paths.forEach(function(path){
-      path = getDirectory(path)
-      if (!/node_modules/.exec(path)){
-        pending += 1
-        exec('git', ['status'], {cwd: path}, function(err, stdout){
-          if (/Untracked|modified/.exec(stdout)){
-            result.push(path)
-            stream.queue(path)
-          }
-          pending -= 1
-          check()
-        })
-      }
-    })
+  var processing = 0
+  var queue = []
+  var pending = 3
+  
+  // only search 4 levels down
+  glob(['.git', '*/.git', '*/*.git', '*/*/*/.git', '*/*/*/*.git'])
 
-    function check(){
-      if (!pending){
+  function glob(patterns){
+    patterns.forEach(function(pattern){
+      new Glob(pattern, {cwd: root}).on('match', add).on('end', function(){
+        pending -= 1
+        check()
+      })
+    })
+  }
+
+  function add(path){
+    path = getDirectory(path)
+    if (!/node_modules/.exec(path)){
+      if (processing > 50){
+        queue.push(path)
+      } else {
+        process(path)
+      }
+    }
+  }
+
+  function process(path){
+    processing += 1
+    exec('git', ['status'], {cwd: path}, function(err, stdout){
+      if (/Untracked|modified/.exec(stdout)){
+        result.push(path)
+        stream.queue(path)
+      }
+      processing -= 1
+      check()
+    })
+  }
+
+  function check(){
+    if (processing == 0){
+      if (queue.length){
+        while (queue.length && processing < 50){
+          process(queue.shift())
+        }
+      } else if (!pending) {
         stream.queue(null)
         if (typeof cb === 'function'){
           cb(null, result) 
         }
       }
     }
-  })
+  }
 
   return stream
 }
