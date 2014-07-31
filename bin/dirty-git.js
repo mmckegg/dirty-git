fdb8f56b5e3d0c1305e499bc49b5e8e1d161f533
jhermsmeier <jhermsmeier@googlemail.com>
2014-07-31 23:59:45 +1200

    Updated API & CLI output (in lack of a better msg)

--- bin/dirty-git.js
+++ bin/dirty-git.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
+var dirty = require( '../' )
+var program = require( 'commander' )
+var package = require( '../package' )
+var format = require( '../lib/format' )
 
-var dirtyGit = require('../index')
-dirtyGit(process.cwd()).on('data', function(data){
-  console.log(data)
-})
+program
+  .version( package.version )
+  .usage( '[options] [path]' )
+  .option( '-i, --ignored', 'include ignored files' )
+  .option( '-u, --untracked', 'include all untracked file paths' )
+  .option( '-s, --staged', 'show stats for staged files' )
+  .option( '-t, --tree', 'show stats for work tree files' )
+  .option( '-d, --depth <n>', '(not implemented)' )
+  .parse( process.argv )
+
+var options = {
+  ignored: !!program.ignored,
+  untracked: !!program.untracked,
+  staged: !!program.staged,
+  tree: !!program.tree,
+  depth: program.depth || 0,
+}
+
+if( !options.staged && !options.tree )
+  options.tree = true
+
+dirty( program.path || process.cwd(), options )
+  .pipe( new format( options ) )
+  .pipe( process.stdout )
