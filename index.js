d3571de91dc48f8e58f862e8943c50430b4681f2
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-05-27 17:48:31 +0200

    Update index.js: Set allowEmpty: true to prevent errors

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var GlobStream = require( 'glob-stream' )
-var StatusStream = require( './lib/status' )
+var GlobStream = require('glob-stream')
+var StatusStream = require('./lib/status')
+var path = require('path')
 
-module.exports = function( dir, options ) {
-  var output = new StatusStream( options )
+module.exports = function (dir, options) {
+  var output = new StatusStream(options)
 
   var depth = options.depth == null ? 3 : parseInt(options.depth)
   var paths = getDepthPaths(depth)
-  
+
   GlobStream.create(paths, {
     cwd: dir || process.cwd(),
-    cwdbase: true
+    cwdbase: true,
+    allowEmpty: true
   }).pipe(output)
 
   return output
 }
 
-function getDepthPaths(depth){
+function getDepthPaths (depth) {
   var result = []
   var currentPath = './'
-  for (var i=0;i<=depth;i++){
-    result.push(currentPath + '.git')
-    currentPath += '*/'
+  for (var i = 0;i <= depth;i++) {
+    result.push(path.join(currentPath, '.git'))
+    currentPath = path.join(currentPath, '*/')
   }
   return result
 }
