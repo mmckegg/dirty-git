5acacac995c3a6532105b61fd1fd795af0b93beb
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-05-27 17:51:02 +0200

    Format with standard-format for consistency

--- bin/dirty-git.js
+++ bin/dirty-git.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-var dirty = require( '../' )
-var program = require( 'commander' )
-var packageInfo = require( '../package' )
-var format = require( '../lib/format' )
-var path = require( 'path' )
+var dirty = require('../')
+var program = require('commander')
+var packageInfo = require('../package')
+var format = require('../lib/format')
+var path = require('path')
 
 program
-  .version( packageInfo.version )
-  .usage( '[options] [path]' )
-  .option( '-i, --ignored', 'include ignored files' )
-  .option( '-u, --untracked', 'include all untracked file paths' )
-  .option( '-l, --symlinks', 'include symlinked repos in results')
-  .option( '-r, --unreleased', 'force include repos that have no current tag')
-  .option( '-s, --staged', 'show stats for staged files' )
-  .option( '-t, --tree', 'show stats for work tree files' )
-  .option( '-d, --depth <n>', 'nested sub folders to search (default: 3)' )
-  .parse( process.argv )
+  .version(packageInfo.version)
+  .usage('[options] [path]')
+  .option('-i, --ignored', 'include ignored files')
+  .option('-u, --untracked', 'include all untracked file paths')
+  .option('-l, --symlinks', 'include symlinked repos in results')
+  .option('-r, --unreleased', 'force include repos that have no current tag')
+  .option('-s, --staged', 'show stats for staged files')
+  .option('-t, --tree', 'show stats for work tree files')
+  .option('-d, --depth <n>', 'nested sub folders to search (default: 3)')
+  .parse(process.argv)
 
 var options = {
   ignored: !!program.ignored,
@@ -27,11 +27,11 @@ var options = {
   symlinks: !!program.symlinks
 }
 
-if( !options.staged && !options.tree )
+if (!options.staged && !options.tree)
   options.tree = true
 
-var dirname = path.resolve( program.args.shift() || process.cwd() )
+var dirname = path.resolve(program.args.shift() || process.cwd())
 
-dirty( dirname, options )
-  .pipe( new format( options ) )
-  .pipe( process.stdout )
+dirty(dirname, options)
+  .pipe(new format(options))
+  .pipe(process.stdout)
